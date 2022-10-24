9edbbbe12d13ed5be6f45b34270c4e25134af2f1
Kaymon McCain <kaymonCodeM@outlook.com>
2022-10-24 18:31:45 -0500

    Just figured out that I can set id to a jwt token!!!

--- MakeMyOrder/src/main/java/retail/orders/MakeMyOrder/Service/TokenServiceImp.java
+++ MakeMyOrder/src/main/java/retail/orders/MakeMyOrder/Service/TokenServiceImp.java
@@ -7,9 +7,12 @@ import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import retail.orders.MakeMyOrder.Entity.User;
+import retail.orders.MakeMyOrder.Repository.UserRepository;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,6 +20,8 @@ public class TokenServiceImp implements TokenService {
 
     @Autowired
     private JwtEncoder encoder;
+    @Autowired
+    private UserRepository userRepository;
 
     //Authenticated User
     //Returns a String of a token value
@@ -24,6 +29,12 @@ public class TokenServiceImp implements TokenService {
     public String generateToken(Authentication authentication){
         //Time stamp
         Instant now = Instant.now();
+
+        String id = "";
+        Optional<User> user = userRepository.findByUsername(authentication.getName());
+        if(user.isPresent()){
+            id += Long.toString(user.get().getUserId());
+        }
 
         //Gets the granted Authorities and creates a scope
         String scope = authentication.getAuthorities().stream()
@@ -37,6 +48,7 @@ public class TokenServiceImp implements TokenService {
                 .expiresAt(now.plus(1, ChronoUnit.HOURS)) //Expires in 1 hour
                 .subject(authentication.getName())
                 .claim("scope",scope) //Is the users scope
+                .id(id)
                 .build();
         return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue(); //Encodes the claim
     }
